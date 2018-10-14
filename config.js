c6a46484039a05f11cb3a6f0963c845760e97363
Salvador Campanella <salvadorcampanella512@gmail.com>
2018-10-14 12:22:54 +0200

    fix configjs

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.export={
 
 	port: process.env.PORT || 3700,
-	db: process.env.MONGODB || 'mongodb://localhost:27017/firefox-bot'
+	db: process.env.MONGODB_URI || 'mongodb://localhost:27017/firefox-bot'
 }
